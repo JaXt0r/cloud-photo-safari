c5baf83c408e5b3b3f60745d8259a9259049b087
Michael Dähnert <michael.daehnert@gmail.com>
2017-07-22 20:11:30 +0200

    swagger cli scripts added.

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 'use strict';
 
 var SwaggerExpress = require('swagger-express-mw');
+const path = require('path');
 var app = require('express')();
+
+
 module.exports = app; // for testing
 
+
+//app.get('*', (req, res) => {
+//  res.sendFile(path.join(__dirname, 'dist/index.html'));
+//});
+
+
 var config = {
-  appRoot: __dirname // required config
+  appRoot: __dirname + "/server",
+  swaggerFile: __dirname + '/server/swagger/swagger.yaml'
 };
+
+
 
 SwaggerExpress.create(config, function(err, swaggerExpress) {
   if (err) { throw err; }
@@ -14,7 +26,7 @@ SwaggerExpress.create(config, function(err, swaggerExpress) {
   // install middleware
   swaggerExpress.register(app);
 
-  var port = process.env.PORT || 10010;
+  var port = process.env.PORT || 3000;
   app.listen(port);
 
   if (swaggerExpress.runner.swagger.paths['/hello']) {
