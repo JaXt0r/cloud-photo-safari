8aa49910b9d7107dcfd9cc64930b72e316c8665f
Michael Dähnert <michael.daehnert@gmail.com>
2017-07-23 16:08:36 +0200

    first image rest-request created.

--- README.md
+++ README.md
@@ -5,9 +5,13 @@ This project was generated with
 * https://scotch.io/tutorials/speed-up-your-restful-api-development-in-node-js-with-swagger
 
 
-
 Used toolchain
 * npm's:
   * @angular/cli - used to generate client skeleton
   * swagger - used to generate server skeleton
   * nodemon - for live reload of nodejs application
+
+
+HowTo use scripts from package.json:
+npm run SKRIPTNAME
+(e.g. npm run swagger-edit)
---
+++ server/api/controllers/image.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var util = require('util');
+
+module.exports = {
+  getImage: getImage
+};
+
+
+function getImage(req, res) {
+  // variables defined in the Swagger document can be referenced using req.swagger.params.{parameter_name}
+
+
+  var response = {};
+
+  var random = Math.floor(Math.random() * 3);
+  var urls = ['http://www.sa-travel.de/wp-content/uploads/sites/8/2015/Fotosafari/19x.jpg',
+            'https://www.benny-rebel.de/wp-content/uploads/2016/06/Benny-Rebel-Fotoreise_Fotosafari-Tansania-Ruanda-Afrika_A2.jpg',
+            'http://www.auto.de/magazin/customs/uploads/auto/2014/06/Miss-Tuning-auf-Foto-Safari-in-Kenia-Kalendershooting-mit-afrikanischem-Flair-misstuning001-620x400.jpg'];
+
+  response.path = urls[random];
+
+
+  res.json(response);
+}
