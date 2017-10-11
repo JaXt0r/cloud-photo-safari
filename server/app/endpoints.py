77288c3b7aa86d56f8abe0a02ef3f5d1bbbefe45
Michael Dähnert <michael.daehnert@gmail.com>
2017-10-11 20:30:55 +0200

    start from scratch with spring and java.

--- server/app/endpoints.py
+++
@@ -1,5 +0,0 @@
-from flask_restful import Resource
-
-class Index(Resource):
-    def get(self):
-        return {'hello': 'world'}
--- server/run.py
+++
@@ -1,7 +0,0 @@
-def main():
-    from app import app
-    app.run()
-
-
-if __name__ == '__main__':
-    main()
