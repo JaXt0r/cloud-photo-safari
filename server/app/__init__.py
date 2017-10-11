92a5741adcdfa55ee8437a800cd16b3055a23ffa
Michael Dähnert <michael.daehnert@gmail.com>
2017-10-11 19:45:54 +0200

    flask_restful added.

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -1,7 +1,12 @@
 from flask import Flask
+from flask_restful import Resource, Api
 
 
 app = Flask(__name__)
+api = Api(app)
 
 
-from . import views
+from . import endpoints
+
+
+api.add_resource(endpoints.Index, '/')
