ee2b03c584101ed941b87d33b794acb64f12e185
Michael Dähnert <michael.daehnert@gmail.com>
2017-12-29 08:17:52 +0100

    Home menu (ActionSheet) added.

--- client/src/pages/home/home.ts
+++ client/src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ActionSheetController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +7,48 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public alertCtrl: ActionSheetController) {
 
   }
 
+
+
+  displayMenu() {
+      let actionSheet = this.alertCtrl.create({
+        title: 'Landschaften',
+        cssClass: 'home-action-sheet-container',
+        buttons: [
+          {
+            icon: 'settings'
+          },
+          {
+            icon: 'folder-open'
+          },
+          {
+            icon: 'shuffle'
+          },
+          {
+            icon: 'pause'
+          },
+          {
+            icon: 'skip-backward'
+          },
+          {
+            icon: 'skip-forward'
+          },
+          {
+            icon: 'copy'
+          },
+          {
+            icon: 'move'
+          },
+          {
+            icon: 'trash'
+          }
+        ]
+      });
+
+      actionSheet.present();
+
+    }
 }
