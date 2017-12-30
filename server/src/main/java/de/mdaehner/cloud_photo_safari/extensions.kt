119da093815c87a7641fc5746415651e8a41bf4f
Michael Dähnert <michael.daehnert@gmail.com>
2017-12-30 18:32:22 +0100

    Extension added.

--- server/src/main/java/de/mdaehner/cloud_photo_safari/extensions.kt
+++ server/src/main/java/de/mdaehner/cloud_photo_safari/extensions.kt
@@ -1,10 +1,18 @@
 package de.mdaehner.cloud_photo_safari
 
 import com.googlecode.jmapper.JMapper
+import com.googlecode.jmapper.conversions.explicit.ConversionPlaceholder.destination
+import com.googlecode.jmapper.conversions.explicit.ConversionPlaceholder.source
 
 
 fun <T, U> MutableCollection<T>.jmap(source: Class<T>, destination: Class<U>): List<U> {
     val mapper = JMapper(destination, source)
 
     return this.map { mapper.getDestination(it) }
+}
+
+fun <T, U> T.jmap(source: Class<T>, destination: Class<U>): U {
+    val mapper = JMapper(destination, source)
+
+    return mapper.getDestination(this)
 }
