f9fd8eafac77339920c5802fc0c36a4fe8e38fb4
Michael Dähnert <michael.daehnert@gmail.com>
2017-12-30 20:56:22 +0100

    NEsted mapping

---
+++ server/src/main/java/de/mdaehner/cloud_photo_safari/model/PhotoData.kt
@@ -0,0 +1,11 @@
+package de.mdaehner.cloud_photo_safari.model
+
+import com.googlecode.jmapper.annotations.JGlobalMap
+
+
+@JGlobalMap
+class PhotoData {
+
+    lateinit var id: String
+
+}
--- server/src/main/java/de/mdaehner/cloud_photo_safari/model/PhotosetData.kt
+++ server/src/main/java/de/mdaehner/cloud_photo_safari/model/PhotosetData.kt
@@ -1,11 +1,13 @@
 package de.mdaehner.cloud_photo_safari.model
 
+import com.googlecode.jmapper.annotations.JGlobalMap
 import com.googlecode.jmapper.annotations.JMap
 
 
+@JGlobalMap
 class PhotosetData {
 
-    @JMap
     lateinit var title: String
+    lateinit var primaryPhoto: PhotoData
 
 }
