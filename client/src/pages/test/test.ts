e6462a3b902d5837cf32618bc271f61d48da1027
Michael Dähnert <michael.daehnert@gmail.com>
2018-01-02 07:30:36 +0100

    First angular animation added.

--- client/src/pages/test/test.ts
+++ client/src/pages/test/test.ts
@@ -1,11 +1,29 @@
 import { Component, ViewChild, OnInit } from '@angular/core';
-import { Events, NavController, ActionSheetController, ModalController } from 'ionic-angular';
+import { NavController, ActionSheetController, ModalController } from 'ionic-angular';
+import { trigger, state, style, animate, transition } from '@angular/animations';
+
 
 @Component({
   selector: 'page-test',
-  templateUrl: 'test.html'
+  templateUrl: 'test.html',
+  animations: [
+    trigger('heroState', [
+      state('false', style({
+        transform: 'translateX(-100%)',
+        opacity: '0'
+      })),
+      state('true', style({
+        transform: 'translateX(0)',
+        opacity: '1'
+      })),
+      transition('false => true', animate('1000ms ease-in')),
+      transition('true => false', animate('1000ms ease-out'))
+    ])
+  ]
 })
 export class TestPage implements OnInit {
+
+  private heroState: boolean = true;
 
   @ViewChild('background1') background1: any;
   @ViewChild('background2') background2: any;
@@ -15,12 +33,15 @@ export class TestPage implements OnInit {
 
   constructor(
     public navCtrl: NavController,
-    public alertCtrl: ActionSheetController, private modalCtrl: ModalController,
-    private events: Events
+    public alertCtrl: ActionSheetController, private modalCtrl: ModalController
   ) {}
 
   ngOnInit() {
 
   }
 
+  toggleState() {
+    this.heroState = !this.heroState;
+    console.log(this.heroState);
+  }
 }
