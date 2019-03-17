4b9f3d6199c611f8eafe743d56a0c786f6376769
Michael Dähnert <michael.daehnert@soprasteria.com>
2019-03-17 17:29:18 +0100

    Spring initializr commit.

---
+++ src/main/kotlin/de/mdaehner/cloud_photo_safari/CloudPhotoSafariApplication.kt
@@ -0,0 +1,11 @@
+package de.mdaehner.cloud_photo_safari
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CloudPhotoSafariApplication
+
+fun main(args: Array<String>) {
+	runApplication<CloudPhotoSafariApplication>(*args)
+}
